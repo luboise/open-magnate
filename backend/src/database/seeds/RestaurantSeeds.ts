e6397f79b0be53ec0a9be49299a3e4aac0fff4ab
luboise <lucas2@live.com.au>
2024-06-24 17:09:55 +1000

    Add ERD and restaurant migrations

---
+++ backend/src/database/seeds/RestaurantSeeds.ts
@@ -0,0 +1,22 @@
+import { RESTAURANT_NAMES } from "../../utils";
+import { Restaurant } from "../entity/Restaurant";
+
+export const seedRestaurant1 = {
+	name: RESTAURANT_NAMES[0]
+} as Partial<Restaurant>;
+export const seedRestaurant2 = {};
+
+export const seedUser2 = {
+	browserId: "test-browser-2",
+	sessionKey: "test-session-key-2",
+	name: "test-player-2"
+};
+
+export const SEED_RESAURANTS = RESTAURANT_NAMES.map(
+	(res) => {
+		return {
+			name: res
+		} as Partial<Restaurant>;
+	}
+);
+
--- shared/RestaurantNames.ts
+++ shared/RestaurantNames.ts
@@ -1,7 +1,12 @@
+export const RESTAURANT_NAMES: string[] = [
+	"Xango Blues Bar",
+	"Santa Maria Pizza",
+	"Fried Geese and Donkey",
+	"Gluttony Inc. Burgers",
+	"Golden Duck Diner",
+	"Siap Faji Bar"
+];
+
 export type RESTAURANT_NAME =
-	| "Xango Blues Bar"
-	| "Santa Maria Pizza"
-	| "Fried Geese and Donkey"
-	| "Gluttony Inc. Burgers"
-	| "Golden Duck Diner"
-	| "Siap Faji Bar";
+	(typeof RESTAURANT_NAMES)[number];
+
