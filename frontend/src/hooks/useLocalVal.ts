66c9c9c9896af0374220e9c0a260244597362411
luboise <lucas2@live.com.au>
2024-07-28 15:22:29 +1000

    Fixed useLocalVal to work with numbers

--- frontend/src/hooks/useLocalVal.ts
+++ frontend/src/hooks/useLocalVal.ts
@@ -1,11 +1,14 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
-function useLocalVal<T extends Object>(
+function useLocalVal<T extends Object | string | number>(
 	localStorageKey: string,
 	defaultValue?: T
 ): [data: T | null, setter: (newVal: T | null) => void] {
 	const { get, set, deleteFromLS } = useLocalStorage();
+	const [val, setVal] = useState<T | null>(
+		(get(localStorageKey) as T) ?? null
+	);
 
 	const setter = useCallback(
 		(newVal: T | null) => {
@@ -19,26 +22,22 @@ function useLocalVal<T extends Object>(
 			set(localStorageKey, newVal);
 
 			setVal(
-				typeof newVal === "string"
-					? newVal
-					: { ...newVal }
+				typeof newVal === "object"
+					? { ...newVal }
+					: newVal
 			);
 		},
 		[localStorageKey]
 	);
 
-	function initialiseState() {
-		const readVal = get(localStorageKey) as T;
-
-		if (!readVal && defaultValue) {
-			setVal(defaultValue);
-			return defaultValue;
-		} else if (readVal) return readVal;
-		else return null;
-	}
-
-	const initialValue = initialiseState();
-	const [val, setVal] = useState<T | null>(initialValue);
+	useEffect(() => {
+		if (!val && defaultValue) {
+			console.debug(
+				`Setting default value for ${localStorageKey} to ${defaultValue}`
+			);
+			setter(defaultValue);
+		}
+	}, []);
 
 	return [val, setter];
 }
